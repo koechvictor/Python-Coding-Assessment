38ee08bd4fcedca1c661b4ac88f01d6646e818ed
koech victor <kipkiruiv408@gmail.com>
2024-08-30 14:05:02 +0000

    function number is even

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 ### 1. `add_numbers`
 This function takes two parameters `num1` and `num2`, and returns their sum.
+
+### 2. `is_even`
+This function takes a single parameter `number` and returns `True` if the number is even, and `False` otherwise.
+
--- index.py
+++ index.py
@@ -3,3 +3,10 @@
 #Returns sum of numbers
 def add_numbers(num1, num2):
     return num1 + num2
+
+#returns True if the number is even, and False otherwise
+def is_even(number):
+    if number%2 == 0:
+        return True
+    else:
+        return False
